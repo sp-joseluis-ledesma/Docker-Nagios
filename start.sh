b1cdc2a421175fef9e79ca3c25c7bd2227ae3e3a
José Luis Ledesma <joseluis.ledesma@socialpoint.es>
2017-05-09 16:36:00 +0200

    building

--- start.sh
+++ start.sh
@@ -19,28 +19,5 @@ if [ ! -f ${NAGIOS_HOME}/etc/htpasswd.users ] ; then
   chown -R nagios.nagios ${NAGIOS_HOME}/etc/htpasswd.users
 fi
 
-shutdown() {
-  echo Shutting Down
-  ls /etc/service | SHELL=/bin/sh parallel --no-notice sv force-stop {}
-  if [ -e /proc/$RUNSVDIR ]; then
-    kill -HUP $RUNSVDIR
-    wait $RUNSVDIR
-  fi
-
-  # give stuff a bit of time to finish
-  sleep 1
-
-  ORPHANS=$(ps -eo pid= | tr -d ' ' | grep -Fxv 1)
-  SHELL=/bin/bash parallel --no-notice 'timeout 5 /bin/bash -c "kill {} && wait {}" || kill -9 {}' ::: $ORPHANS 2> /dev/null
-  exit
-}
-
-exec runsvdir -P /etc/service &
-RUNSVDIR=$!
-echo "Started runsvdir, PID is $RUNSVDIR"
-
-trap shutdown SIGTERM SIGHUP SIGINT
-wait $RUNSVDIR
-
-shutdown
+/opt/nagios/bin/nagios /opt/nagios/etc/nagios.cfg
 
